3a4c40a8fd0f7d63f18b3832fbee2d18aa6e60aa
ngtrdai <19146146@student.hcmute.edu.vn>
2021-08-21 19:58:26 +0700

    fix

--- runserver.py
+++ runserver.py
@@ -4,10 +4,10 @@ app = Create_App()
 
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT,debug=True)
-    # app.run(debug=True)
+    # HOST = environ.get('SERVER_HOST', 'localhost')
+    # try:
+    #     PORT = int(environ.get('SERVER_PORT', '5555'))
+    # except ValueError:
+    #     PORT = 5555
+    # app.run(HOST, PORT,debug=True)
+    app.run(debug=True)
